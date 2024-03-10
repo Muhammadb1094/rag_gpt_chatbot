a07a06fba66810b99867ffc32267faf1d580c30f
Muhammad Bilal <bilalakbar1094@gmail.com>
2024-03-10 05:16:51 +0500

    langchain conversation work completed

--- chat/llm_gpt.py
+++ chat/llm_gpt.py
@@ -1,31 +1,42 @@
 from chat.models import Message
 from django.conf import settings
-from openai import OpenAI
+from langchain import OpenAI, ConversationChain
+from langchain.memory import ConversationBufferMemory
 
+
+# def get_llm_response(query, conver_id):
+#     from openai import OpenAI
+#     client = OpenAI(api_key=settings.OPENAI_API_KEY)
+
+#     response = client.chat.completions.create(
+#         model="gpt-3.5-turbo",
+#         messages=[
+#             {"role": "system", "content": "You are a helpful assistant."},
+#             {"role": "user", "content": query},
+#             ]
+#     )
+#     return response.choices[0].message.content
 
 
 def get_llm_response(query, conver_id):
+    memory = ConversationBufferMemory()
+    try:
+        if not conver_id == "" or not conver_id is None:
+            memorybuffer = Message.objects.filter(
+                conversation_id=conver_id).order_by('-created_at')
 
-    client = OpenAI(api_key=settings.OPENAI_API_KEY)
+            for item in memorybuffer:
+                memory.chat_memory.add_user_message(item.query)
+                memory.chat_memory.add_ai_message(item.response)
+            memory.load_memory_variables({})
+    except Exception as e:
+        print(e)
 
-    response = client.chat.completions.create(
-        model="gpt-3.5-turbo",
-        messages=[
-            {"role": "system", "content": "You are a helpful assistant."},
-            {"role": "user", "content": query},
-            ]
-    )
-    return response.choices[0].message.content
-
-
-def test(query, conver_id): 
-    from langchain import OpenAI as o, ConversationChain
-
-    llm = o(temperature=0, openai_api_key=settings.OPENAI_API_KEY,
-                model_name="gpt-3.5-turbo")
+    llm = OpenAI(temperature=0, model_name="gpt-3.5-turbo")
     conversation = ConversationChain(
         llm=llm,
         verbose=True,
+        memory=memory,
     )
-    output = conversation.predict(input="Who is the prime Minister of Pakistan?")
+    output = conversation.predict(input=query)
     return output
